d22e3ab880530ee5262dc202d2724fa1bb4ffb1f
Brice Venayre <brice.venayre@gmail.com>
2023-01-12 17:10:39 +0100

    Add admin - authentication

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from authentication.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    """Class managing custom users in admin panel."""
+
+    list_display = ['id', 'first_name', 'last_name', "role"]
+    list_filter = ["role"]
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'first_name', 'last_name', 'role', 'password1', 'password2')}
+        ),
+    )
+
+    search_fields = ['first_name']
+    ordering = ['first_name']
