94aa63976b59e217c05e1f5ff3b92e1dcfec1697
Brice Venayre <brice.venayre@gmail.com>
2023-01-19 17:20:02 +0100

    Add simple JWT and urls

--- src/eventmanager/settings.py
+++ src/eventmanager/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
     'authentication',
     'crm_api',
 ]
@@ -127,3 +128,18 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "authentication.CustomUser"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+}
--- src/eventmanager/urls.py
+++ src/eventmanager/urls.py
@@ -14,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_nested import routers
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
+from crm_api import views
+
+router = routers.SimpleRouter()
+router.register(r'clients', views.ClientViewset, basename="client")
+router.register(r'users', views.CustomUserViewset, basename="user")
+
+clients_router = routers.NestedSimpleRouter(router, r'clients', lookup='client')
+clients_router.register(r'contracts', views.ContractViewset, basename='project-contracts')
+
+contracts_router = routers.NestedSimpleRouter(clients_router, r'contracts', lookup='contract')
+contracts_router.register(r'events', views.EventViewset, basename='contract-events')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(r'', include(router.urls)),
+    path(r'', include(clients_router.urls)),
+    path(r'', include(contracts_router.urls)),
 ]
