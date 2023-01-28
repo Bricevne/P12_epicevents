cdd9dd556f21ac36875ade147a5b623bd9bbcb13
Brice Venayre <brice.venayre@gmail.com>
2023-01-28 16:34:19 +0100

    Add admin customization

--- src/eventmanager/settings.py
+++ src/eventmanager/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'eventmanager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "eventmanager/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
